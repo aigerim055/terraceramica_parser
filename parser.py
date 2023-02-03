118ef71692ce8e40b977c2ecddae09cf87835132
aigerim055 <omurzakova727@gmail.com>
2023-02-03 17:23:39 +0600

    почти готово(

--- parser.py
+++ parser.py
@@ -54,22 +54,173 @@ data = {
 
 
 
-async def get_page_data(session):
-    
-    
-    url = 'https://terraceramica.ru'
-    response = await session.get(url=url, data=data, headers=headers, cookies=cookies)
-    soup = BeautifulSoup(await response.text(), "lxml")
-    # print(soup)
-    # links = []
-    # cards: ResultSet = soup.find_all('div', class_="col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 d-flex")
-    # for link in cards:
-    #     link = link.find('a', class_='card_any').get('href')[0:17]
-    #     # print(link)
-    #     link = f'{url}{link}'
-    #     print(link)
-    # links.append(link)
+async def get_page_data():
+    urls = ['https://terraceramica.ru/product/16443119']
+    data = []
+    for url in urls:
 
+        async with aiohttp.ClientSession(trust_env=True) as session:
+            response = await session.get(url=url, data=data, headers=headers, cookies=cookies)
+            soup = BeautifulSoup(await response.text(), "lxml")
+
+
+            try:
+                code = soup.find('div', class_='item_details_article').find('div', class_='value').text
+            except AttributeError:
+                code = ''
+
+            
+            try:
+                title = soup.find('div', class_="col-12 col-md-6 col-lg-7  d-flex  custom").find('h1').text
+            except AttributeError:
+                title = '' 
+
+            try:
+                price = soup.find('div', class_="item_details_price_val").text
+            except AttributeError:
+                price = ''
+
+            try:
+                price_q = soup.find('div', class_="item_details_price_val").find('span').text
+            except AttributeError:
+                price_q = ''
+
+            try:
+                price_without_discount = soup.find('div',class_="item_details_price_val_base").find('div', class_='value').text
+            except AttributeError:
+                price_without_discount = ''
+
+            try:
+                price_without_discount_q = soup.find('div',class_="item_details_price_val_base").find('div', class_='value').find('span').text
+            except AttributeError:
+                price_without_discount_q = ''
+
+            try:
+                info = soup.find('div',class_="item_details_info").find_all('div', class_='item')#.find('div', class_='value').text
+            except AttributeError:
+                info = ''
+
+            try:
+                articul = info[0].find('div', class_='value').text
+            except AttributeError:
+                articul = ''
+
+            try:
+                collection = info[1].find('div', class_='value').text
+            except AttributeError:
+                collection = ''
+            
+            try:
+                manufacture = info[2].find('div', class_='value').text
+            except AttributeError:
+                manufacture = ''
+
+            try:
+                color = info[3].find('div', class_='value').text
+            except AttributeError:
+                color = ''
+
+            try:
+                size = info[4].find('div', class_='value').text
+            except AttributeError:
+                size = ''
+
+            try:
+                thickness = info[5].find('div', class_='value').text
+            except AttributeError:
+                thickness = ''
+            
+
+            try:
+                in_stock_cards = soup.find('div', class_='table_container').find_all('tr')
+            except AttributeError:
+                in_stock_cards = ''
+
+            try:
+                in_stock_1 = in_stock_cards[0].find('div', class_='stock').text
+            except AttributeError:
+                in_stock_1 = ''
+
+            try:
+                in_stock_1_tone = in_stock_cards[0].find('div', class_='tone').find('span').text
+            except AttributeError:
+                in_stock_1_tone = ''            
+
+            try:
+                in_stock_1_caliber = in_stock_cards[0].find('div', class_='caliber').find('span').text
+            except AttributeError:
+                in_stock_1_caliber = ''  
+
+            try:
+                in_stock_1_cost = in_stock_cards[0].find('div', class_='cost').find('span').text # остаток
+            except AttributeError:
+                in_stock_1_cost = '' 
+
+            try:
+                in_stock_2 = in_stock_cards[1].find('div', class_='stock').text
+            except AttributeError:
+                in_stock_2 = ''
+
+            try:
+                in_stock_2_tone = in_stock_cards[1].find('div', class_='tone').find('span').text
+            except AttributeError:
+                in_stock_2_tone = ''            
+
+            try:
+                in_stock_2_caliber = in_stock_cards[1].find('div', class_='caliber').find('span').text
+            except AttributeError:
+                in_stock_2_caliber = ''    
+
+            try:
+                in_stock_2_cost = in_stock_cards[1].find('div', class_='cost').find('span').text # остаток
+            except AttributeError:
+                in_stock_2_cost = '' 
+
+            try:
+                in_stock_3 = in_stock_cards[2].find('div', class_='stock').text
+            except AttributeError:
+                in_stock_3 = ''
+
+            try:
+                in_stock_3_tone = in_stock_cards[2].find('div', class_='tone').find('span').text
+            except AttributeError:
+                in_stock_3_tone = ''            
+
+            try:
+                in_stock_3_caliber = in_stock_cards[2].find('div', class_='caliber').find('span').text
+            except AttributeError:
+                in_stock_3_caliber = '' 
+
+            try:
+                in_stock_3_cost = in_stock_cards[2].find('div', class_='cost').find('span').text # остаток
+            except AttributeError:
+                in_stock_3_cost = '' 
+
+            try:
+                in_stock_4 = in_stock_cards[3].find('div', class_='stock').text
+            except AttributeError:
+                in_stock_4 = ''
+
+            try:
+                in_stock_4_tone = in_stock_cards[3].find('div', class_='tone').find('span').text
+            except AttributeError:
+                in_stock_4_tone = ''            
+
+            try:
+                in_stock_4_caliber = in_stock_cards[3].find('div', class_='caliber').find('span').text
+            except AttributeError:
+                in_stock_4_caliber = '' 
+
+            try:
+                in_stock_4_cost = in_stock_cards[3].find('div', class_='cost').find('span').text # остаток
+            except AttributeError:
+                in_stock_4_cost = ''     
+
+            
+
+        
+                    
+            print(in_stock_4_cost)
 
 
 async def gather_data():
@@ -78,74 +229,151 @@ async def gather_data():
 
 
     async with aiohttp.ClientSession(trust_env=True) as session:
-        response = await session.get(url=url, data=data, headers=headers, cookies=cookies)
-        soup = BeautifulSoup(await response.text(), "lxml")
-        # print(soup)
-        links = []
-        cards: ResultSet = soup.find_all('div', class_="col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 d-flex")
-        for link in cards:
-            link = link.find('a', class_='card_any').get('href')[0:17]
-            # print(link)
-            link = f'{url}{link}'
-            # print(link)
-            links.append(link)
-        # print(links)
-        links2 = []
-        for link in links:
-            async with aiohttp.ClientSession(trust_env=True) as session:
-                response = await session.get(url=link, data=data, headers=headers, cookies=cookies)
-                soup = BeautifulSoup(await response.text(), "lxml")
-                # print(soup)
-                cards = soup.find_all('div', class_='col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 d-flex')
+        # response = await session.get(url=url, data=data, headers=headers, cookies=cookies)
+        # soup = BeautifulSoup(await response.text(), "lxml")
+        # links = []
+        # cards: ResultSet = soup.find_all('div', class_="col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 d-flex")
+        # for link in cards:
+        #     link = link.find('a', class_='card_any').get('href')[0:17]
+        #     link = f'{url}{link}'
+        #     links.append(link)
+
+
+        # links2 = []
+        # for link in links:
+        #     async with aiohttp.ClientSession(trust_env=True) as session:
+        #         response = await session.get(url=link, data=data, headers=headers, cookies=cookies)
+        #         soup = BeautifulSoup(await response.text(), "lxml")
+        #         # print(soup)
+        #         cards = soup.find_all('div', class_='col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 d-flex')
             
-                for link in cards:
-                    # print(link)
-                    try:
-                        link = link.find('a', class_='card_any').get('href')
-                    except AttributeError:
+        #         for link in cards:
+        #             # print(link)
+        #             try:
+        #                 link = link.find('a', class_='card_any').get('href')
+        #             except AttributeError:
                         
-                        link = link.find('a', class_='card_producer').get('href')
+        #                 link = link.find('a', class_='card_producer').get('href')
                         
-                    link = f'{url}{link}'
-                    # print(link)
-                    links2.append(link)
-        # print(links2)
-        links3 = []
-        for link in links2:
-            async with aiohttp.ClientSession(trust_env=True) as session:
-                response = await session.get(url=link, data=data, headers=headers, cookies=cookies)
-                soup = BeautifulSoup(await response.text(), "lxml")
-                cards = soup.find_all('div', class_='col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 d-flex')
+        #             link = f'{url}{link}'
+        #             # print(link + ' - FIRST PAGE URLS')
+        #             links2.append(link)
+        # # print(links2)
 
-                for link in cards:
-                    try:
-                        link = link.find('a', calss_='card_collection').get('href')
-                    except AttributeError:
-                        try:
-                            link = link.find('a', class_='card_producer').get('href')
-                        except AttributeError:
-                            try:
-                                link = link.find('a', class_="card_collection").get('href')
-                            except AttributeError:
-                                link = link.find('a', class_="card_any").get('href')
-                    link = f'{url}{link}'
-                    print(link)
+        # links3 = []
+        
+        # for link in links2:
+        #     async with aiohttp.ClientSession(trust_env=True) as session:
+        #         response = await session.get(url=link, data=data, headers=headers, cookies=cookies)
+        #         soup = BeautifulSoup(await response.text(), "lxml")
+        #         cards = soup.find_all('div', class_='col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 d-flex')
+        #         a = 0
+        #         for link in cards:
+                    
+        #             try:
+        #                 link = link.find('a', calss_='card_collection').get('href')
+        #             except AttributeError:
+        #                 try:
+        #                     link = link.find('a', class_='card_producer').get('href')
+        #                 except AttributeError:
+        #                     try:
+        #                         link = link.find('a', class_="card_collection").get('href')
+        #                     except AttributeError:
+        #                         link = link.find('a', class_="card_any").get('href')
+        #             link = f'{url}{link}'
+        #             print(link + ' - SECOND PAGE')
+        #             links3.append(link)
+
+        # # print(links3)
+        # links4 = []
+        # for linkk in links3:
+        #     async with aiohttp.ClientSession(trust_env=True) as session:
+        #         response = await session.get(url=linkk, data=data, headers=headers, cookies=cookies)
+        #         soup = BeautifulSoup(await response.text(), "lxml")
+                
+        #         cards = soup.find_all('div', class_='col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-3 d-flex')
+        #         # print(cards)
+        #         for link in cards:
+        #             try:
+        #                 link = link.find('a', class_='card_collection').get('href')
+        #                 # link = f'{url}{link}'
+        #                 # print(link + '    BBBBBBBBBBBBBBBBBBBBBBBBBBBB')
+        #                 # link = f'{url}{link}'
+        #             except AttributeError:
+        #                 try:
+        #             #    print(link)
+        #                     link = link.find('a', class_='card_any').get('href')
+        #                     # link = f'{url}{link}'
+        #                     # print(link + '    AAAAAAAAAAAAAAAAAAAAAA')
+        #                 except AttributeError:
+        #                     # print(link)
+        #                     link = link.find('a', class_='card_producer').get('href')
+        #                     # link = f'{url}{link}'
+        #                     # print(link + '    VVVVVVVVVVVVVVVVVVVVVVVV')
+        #             link = f'{url}{link}'
+        #             # print(link + ' - done links i hope')
+        #             links4.append(link)
+        #             print(link + ' - THIRD PAGE')
+
+        # # print(links4)
+        # # print(products_page_urls)
+        # # print(links4)
 
 
-               
+        # all_cards_links = []
+        # for link in links3:
+        #     async with aiohttp.ClientSession(trust_env=True) as session:
+        #         response = await session.get(url=link, data=data, headers=headers, cookies=cookies)
+        #         soup = BeautifulSoup(await response.text(), "lxml")
+                
+        #         cards = soup.find_all('div', class_="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-3 d-flex")
+        #         # print(cards)
+        #         for card in cards:
+        #             try:
+        #                 card_link = card.find('a', class_='info_title').get('href')
+        #                 card_link = f'{url}{card_link}'
+        #                 print(card_link + '   - PRODUCT LINK 1')
+        #                 all_cards_links.append(card_link)
+        #             except AttributeError:
+        #                 print('error')
+                
+                
 
+        # # print(all_cards_links)
 
+        # for link in links4:
+        #     async with aiohttp.ClientSession(trust_env=True) as session:
+        #         response = await session.get(url=link, data=data, headers=headers, cookies=cookies)
+        #         soup = BeautifulSoup(await response.text(), "lxml")
+                
+        #         cards = soup.find_all('div', class_="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-3 d-flex")
+        #         # print(cards)
+        #         for card in cards:
+        #             try:
+        #                 card_link = card.find('a', class_='info_title').get('href')
+        #                 card_link = f'{url}{card_link}'
+        #                 print(card_link + '   - PRODUCT LINK 2')
+        #                 all_cards_links.append(card_link)
+        #             except AttributeError:
+        #                 print('error')
+        
             
-   
-        task = asyncio.create_task(get_page_data(session))
+
+        task = asyncio.create_task(get_page_data())
         await asyncio.gather(task)
 
 
 def main():
     asyncio.run(gather_data())
-    # finish_time = time.time() - start_time
-    # print(f"Затраченное на работу скрипта время: {finish_time}")
+    finish_time = time.time() - start_time
+    print(f"Затраченное на работу скрипта время: {finish_time}")
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# TODO:
+# в наличии -> остаток поделить значение и ед измерения
